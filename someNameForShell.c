3125983f76e25e69b7ee038054535445bce5b373
Harman Dhillon <harmandhi22@gmail.com>
2019-02-27 21:02:30 -0500

    exit doesnt work, string parsed

--- someNameForShell.c
+++ someNameForShell.c
@@ -10,27 +10,32 @@
 #define SHELLNAME "someNameForShell"
 
 void shellIndicator();
-void lowercaseUserInput(char *userInput, int userInputLength);
+void lowercaseUserInput(char *rawUserInput, int userInputLength);
+void parseUserInput(char *rawUserInput,char *userInputArray);
 void changeDirectory();
+
 
 void userInputLoop(){
 	size_t userInputLength = 128;
-	char *userInput;
+	int maxTokens = 32;
+	char *userInputTokenArray[maxTokens];
+	char *rawUserInput;
 	char exit[4] = "exit";
 	
 	shellIndicator();
-	getline (&userInput, &userInputLength, stdin);
-	lowercaseUserInput(userInput,userInputLength);
+	getline (&rawUserInput, &userInputLength, stdin);
+	lowercaseUserInput(rawUserInput,userInputLength);
+	parseUserInput(rawUserInput, userInputTokenArray);
 
-	printf ("	You entered: %s\n", userInput);
+	//printf ("	You entered: %s\n", rawUserInput);
 	
-	while(strcmp(exit,userInput)){
+	while(strcmp(exit,rawUserInput)){
 		
 		shellIndicator();
-		getline (&userInput, &userInputLength, stdin);
-		lowercaseUserInput(userInput,userInputLength);
-		
-		printf ("	You entered: %s\n", userInput);
+		getline (&rawUserInput, &userInputLength, stdin);
+		lowercaseUserInput(rawUserInput,userInputLength);
+		parseUserInput(rawUserInput, userInputTokenArray);
+		//printf ("	You entered: %s\n", rawUserInput);
 	}
 }
 
@@ -38,12 +43,20 @@ void shellIndicator(){
 	printf ("%s %s", SHELLNAME, INDICATOR);
 }
 
-void lowercaseUserInput(char *userInput, int userInputLength){
+void lowercaseUserInput(char *rawUserInput, int userInputLength){
 		//to lower case userinput
 		int i;
 		for(i = 0; i<userInputLength; i++){
-			userInput[i] = tolower(userInput[i]);
+			rawUserInput[i] = tolower(rawUserInput[i]);
 		}
+}
+
+void parseUserInput(char *rawUserInput,char *userInputArray){
+	userInputArray = strtok(rawUserInput, " ");
+	while (userInputArray != NULL) {
+		printf("%s\n", userInputArray);
+		userInputArray= strtok(NULL, " ,");
+	}
 }
 
 void changeDirectory(){
@@ -53,6 +66,5 @@ void changeDirectory(){
 int main(int argc, char *argv[])
 {
 	userInputLoop();
-	
    return 0;
 }
