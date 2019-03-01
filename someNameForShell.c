6ddc56a3f21ceac1715bc34bfc45fda3b3c4ce8d
Harman Dhillon <harmandhi22@gmail.com>
2019-03-01 15:22:41 -0500

    exit works and the user input is parsed in a array
    the print array causes a core dump because it goes out of bounds

--- someNameForShell.c
+++ someNameForShell.c
@@ -11,32 +11,31 @@
 
 void shellIndicator();
 void lowercaseUserInput(char *rawUserInput, int userInputLength);
-void parseUserInput(char *rawUserInput,char *userInputArray);
+void parseUserInput(char *rawUserInput,char **userInputArray);
 void changeDirectory();
 
 
 void userInputLoop(){
 	size_t userInputLength = 128;
-	int maxTokens = 32;
+	int maxTokens = 3;
 	char *userInputTokenArray[maxTokens];
 	char *rawUserInput;
-	char exit[4] = "exit";
-	
-	shellIndicator();
-	getline (&rawUserInput, &userInputLength, stdin);
-	lowercaseUserInput(rawUserInput,userInputLength);
-	parseUserInput(rawUserInput, userInputTokenArray);
+	char *exit = "exit";//char *exit = "exit";
 
 	//printf ("	You entered: %s\n", rawUserInput);
 	
-	while(strcmp(exit,rawUserInput)){
-		
-		shellIndicator();
+	do {
+	shellIndicator();
 		getline (&rawUserInput, &userInputLength, stdin);
 		lowercaseUserInput(rawUserInput,userInputLength);
+		//printf ("before the parsing: %s", rawUserInput);
 		parseUserInput(rawUserInput, userInputTokenArray);
-		//printf ("	You entered: %s\n", rawUserInput);
-	}
+		
+		int i;
+		for (i = 0; i < 3; ++i) 
+				printf("%s\n", userInputTokenArray[i]);
+	} while( strcmp(exit, userInputTokenArray[0]) != 0 );
+	//loop runs as long as the first word isnt exit
 }
 
 void shellIndicator(){
@@ -51,11 +50,15 @@ void lowercaseUserInput(char *rawUserInput, int userInputLength){
 		}
 }
 
-void parseUserInput(char *rawUserInput,char *userInputArray){
-	userInputArray = strtok(rawUserInput, " ");
-	while (userInputArray != NULL) {
-		printf("%s\n", userInputArray);
-		userInputArray= strtok(NULL, " ,");
+void parseUserInput(char *rawUserInput,char **userInputArray){
+
+	int i = 0;
+	char *temp = strtok (rawUserInput, " ");
+
+	while (temp != NULL)
+	{
+		userInputArray[i++] = temp;
+		temp = strtok (NULL, " ");
 	}
 }
 
